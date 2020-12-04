1b73fd0b84fe22da98eca7723b8275a99077dfa1
Jizga <jizga93@gmail.com>
2020-12-04 22:25:10 +0100

    console.log removed

--- app.js
+++ app.js
@@ -74,5 +74,5 @@ function game() {
   document.getElementById("messageGame").innerHTML = msg;
   document.getElementById("contadorP").innerHTML = contadorP;
   document.getElementById("contadorC").innerHTML = contadorC;
-  console.log(contador);
+ 
 }
